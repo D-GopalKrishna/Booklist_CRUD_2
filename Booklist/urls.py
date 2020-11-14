a2a1d7e945be7315350fa95df8c7b770acb4c2e6
DGK <nikhil0223@gmail.com>
2020-11-14 10:28:16 +0530

    changes in Requirements.txt 7

--- Booklist/urls.py
+++ Booklist/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', include('Bookdetails.urls'))
+    path('', include('Bookdetails.urls'))
 ]
